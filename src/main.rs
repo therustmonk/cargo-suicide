dd587b19d91215f057e72d1f9e0814156fb406f4
Denis Kolodin <deniskolodin@gmail.com>
2019-04-01 08:03:08 +0900

    Release 0.1.1 - god mode
    Use `--god` option if you feel the Lord in you.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
-use std::io::{self, Write};
+use std::env;
+use std::io::{self, Result, Write};
 use std::process::Command;
 
-fn main() {
+fn main() -> Result<()> {
     let output = Command::new("cargo")
         .args(&["build"])
         .env("RUSTFLAGS", "-D warnings")
-        .output()
-        .expect("failed to execute process");
+        .output()?;
     io::stdout().write_all(&output.stdout).unwrap();
     io::stderr().write_all(&output.stderr).unwrap();
     if !output.status.success() {
-        Command::new("git")
-            .args(&["reset", "--hard"])
-            .status()
-            .expect("failed to execute process");
+        let status = Command::new("git").args(&["reset", "--hard"]).status()?;
+        if !status.success() {
+            println!("I don't play like that! Install 'git', wimp!");
+        }
+        if env::args().any(|x| x == "--god") {
+            kill("emacs");
+            kill("vim");
+        }
     }
+    Ok(())
+}
+
+fn kill(cmd: &str) {
+    Command::new("killall").args(&["-9", cmd]).status().ok();
 }
